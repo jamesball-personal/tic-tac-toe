6f4594d460ec5933a612329faf8c4afee1c87f82
jamesball-personal <jamesball.personal@gmail.com>
2020-09-02 11:09:12 +0100

    Initialise Gradle project

---
+++ src/test/java/com/jamesball/tictactoe/TicTacToeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jamesball.tictactoe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTacToeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
