9625674b5081a1c0f0c6a33ba9e3fabc5043d931
jamesball-personal <jamesball.personal@gmail.com>
2020-09-05 14:24:30 +0100

    Player 1 can mark a space on the board

--- src/main/java/com/jamesball/tictactoe/PlayerMark.java
+++ src/main/java/com/jamesball/tictactoe/PlayerMark.java
@@ -2,7 +2,24 @@ package com.jamesball.tictactoe;
 
 public enum PlayerMark {
 
-    X,
-    O
+    X(1),
+    O(2);
 
+    private int playerNumber;
+
+    PlayerMark(int playerNumber) {
+        this.playerNumber = playerNumber;
+    }
+
+    public int getPlayerNumber() {
+        return this.playerNumber;
+    }
+
+    @Override
+    public String toString() {
+        return "PlayerMark{" +
+                "mark=" + this.name() + ", " +
+                "playerNumber=" + this.playerNumber +
+                '}';
+    }
 }
